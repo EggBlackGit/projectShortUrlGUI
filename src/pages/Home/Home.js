4a7b0fb1ac8527aacdd4e6007b5a23a94cd2dcd4
EggBlackGit <85241992+EggBlackGit@users.noreply.github.com>
2023-01-06 02:50:31 +0700

    update home set axios

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,10 +10,10 @@ function Home() {
     const [dataUrl, setDataUrl] = useState("");
 
     useEffect(() => {
-        // Axios.get("https://projectshorturlws.onrender.com/echo").then((response) => { // ปลุก ws ให้พร้อมใช้งาน
-        Axios.get("http://localhost:3001/echo").then((response) => { // ปลุก ws ให้พร้อมใช้งาน
+        Axios.get("https://projectshorturlws.onrender.com/echo").then((response) => { // ปลุก ws ให้พร้อมใช้งาน
+        // Axios.get("http://localhost:3001/echo").then((response) => { // ปลุก ws ให้พร้อมใช้งาน
         })
-        console.log(dataUrl);
+        // console.log(dataUrl);
     }, []);
 
     const Toast = Swal.mixin({
@@ -29,9 +29,7 @@ function Home() {
     });
 
     const doShortUrl = () => {
-        console.log(urlFull);
-        // Axios.post("https://projectshorturlws.onrender.com/shortUrl", {
-
+        // console.log(urlFull);
         if (urlFull == null || urlFull == "") {
             Swal.fire({
                 icon: 'warning',
@@ -42,10 +40,11 @@ function Home() {
 
         let checkUrlFormat = isValidHttpUrl(urlFull);
         if (checkUrlFormat) {
-            Axios.post("http://localhost:3001/shortUrl", {
+            Axios.post("https://projectshorturlws.onrender.com/shortUrl", {
+            // Axios.post("http://localhost:3001/shortUrl", {
                 urlFull: urlFull
             }).then((response) => {
-                console.log(response);
+                // console.log(response);
                 setDataUrl(response.data);
             })
         } else {
@@ -60,11 +59,11 @@ function Home() {
     const checkShortUrl = () => {
         let shortId = dataUrl.shortId;
         console.log(shortId);
-        // Axios.get("https://projectshorturlws.onrender.com/"+shortId).then((response) => {
-        Axios.get("http://localhost:3001/updateCount/" + shortId).then((response) => {
-            console.log(response);
+        Axios.get("https://projectshorturlws.onrender.com/updateCount/"+shortId).then((response) => {
+        // Axios.get("http://localhost:3001/updateCount/" + shortId).then((response) => {
+            // console.log(response);
             setDataUrl(response.data);
-            console.log(response.data.fullUrl);
+            // console.log(response.data.fullUrl);
             window.open(response.data.fullUrl, '_blank');
         })
     }
@@ -135,31 +134,6 @@ function Home() {
                     </div>
                 </div>
             </div> : ('')}
-
-            {/* <div className="col d-flex justify-content-center">
-                <div className="card cardResult">
-                    <div className="card-body">
-                        <div className="resultText">
-                            <div className="origin-link">
-                                {dataUrl.fullUrl}
-                            </div>
-                            <div className="short-link">
-                                <a href="#" onClick={() => checkShortUrl()}>{dataUrl.shortUrl}</a>
-                            </div>
-                            <div className="count-link">
-                                <h6>click {dataUrl.clicks}</h6>
-                            </div>
-                            <div className="copy-link">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clipboard" viewBox="0 0 16 16" onClick={() => copyToClipboard(dataUrl.shortUrl)}>
-                                    <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
-                                    <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
-                                </svg>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div> */}
 
         </div>
     </div>);
